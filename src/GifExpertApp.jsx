707714a48b2c03d6389128abefe1ce742a9dc9e3
tadescalzo <ta.descalzo@gmail.com>
2023-02-25 19:52:32 -0300

    logica de busqueda

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
+import { AddCategory } from "./AddCategory"
+
 export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['One Punch','Dragon Ball'])
+
+    const onAddCategory = (newCategory) =>{
+        setCategories([...categories,newCategory])
+    }
+
   return (
     <>
-    {/* Titulo */}
+
     <h1>GifExpertApp</h1>
 
-    {/* Input para buscar gifs */}
+    {/*Recibe el valor del input y se agrega al estado de categories*/}
+    <AddCategory onNewCategory ={(event)=> onAddCategory(event) } />
+    
+    <ol>
+    {categories.map( category =>{
+        return <li key={category}>{category}</li>})}
+    </ol>
 
-    {/* Listado de gifs */}
-        {/* Gif Items */}
     </>
   )
 }
