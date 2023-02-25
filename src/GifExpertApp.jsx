a01ee7094010d2c7491584edadb47fea801df183
tadescalzo <ta.descalzo@gmail.com>
2023-02-25 17:31:07 -0300

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+export const GifExpertApp = () => {
+  return (
+    <>
+    {/* Titulo */}
+    <h1>GifExpertApp</h1>
+
+    {/* Input para buscar gifs */}
+
+    {/* Listado de gifs */}
+        {/* Gif Items */}
+    </>
+  )
+}
