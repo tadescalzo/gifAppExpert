323db513b22ba8209af7aebae01a91cfcfe492f3
tadescalzo <ta.descalzo@gmail.com>
2023-02-25 23:41:14 -0300

    peticion de gifs a api

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react"
 import { AddCategory } from "./AddCategory"
+import { GifGrid } from "./GifGrid"
 
 export const GifExpertApp = () => {
     
-    const [categories, setCategories] = useState(['One Punch','Dragon Ball'])
+    const [categories, setCategories] = useState(['Dragon Ball'])
 
     const onAddCategory = (newCategory) =>{
+        if (categories.includes(newCategory)) return;
         setCategories([...categories,newCategory])
+        
+        
     }
 
   return (
     <>
-
+    {/* Titulo */}
     <h1>GifExpertApp</h1>
 
     {/*Recibe el valor del input y se agrega al estado de categories*/}
     <AddCategory onNewCategory ={(event)=> onAddCategory(event) } />
     
-    <ol>
-    {categories.map( category =>{
-        return <li key={category}>{category}</li>})}
-    </ol>
+    {/* Se mapean las categorias ingresadas */}
+    {
+        categories.map( category =>(
+            <GifGrid key={category} category={category} />
+        ))
+    }
+
 
     </>
   )
